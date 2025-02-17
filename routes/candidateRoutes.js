678cac64db130fd009ffe213dfe91c6b4ab8de38
sachin kumar <sachinroysky@gmail.com>
2025-02-17 18:33:16 +0530

    Added VoteCount and AllCandidates functions

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -2,9 +2,9 @@ const express= require('express');
 const router = express.Router();
 const {jwtAuthMiddleware} = require('../jwt');
 const candidateController = require("../controllers/candidateController");
-//router.post('/login',userController.Login);
+
 router.post('/add-candidate',jwtAuthMiddleware,candidateController.Register);
-// router.get('/protect',jwtAuthMiddleware,(req,res)=>{
-//      res.send("Welcome to protected route");
-// })
+router.get('/',candidateController.AllCandidates);
+router.get('/vote/count',candidateController.VoteCount);
+
 module.exports = router;
